4c4a2a12298f192c34af65a2bc2684e90c4351ad
montd <dzejdzej.j@gmail.com>
2021-10-08 11:58:46 +0200

    Get word from MIT wordlist, fix errors

--- hangman.py
+++ hangman.py
@@ -1,25 +1,49 @@
 import random
-life = 6
-success = False
-words = ('baba','bubu')
-word = words[random.randrange(0,len(words))]
-wordObf = ['_' for i in list(word[:])]
+import urllib.request
 
-while life>0 and not(success):
-    guess = input("Guess the letter: ")
-    print('\n')
-    if set(guess).issubset(set(word)):
-        counter1 = 0
-        for i in word:
-            if i == guess: 
-                wordObf[counter1] = i
-            counter1+=1
-        print("Good guess!")
-        print("".join([i+" " for i in wordObf]))
-    if wordObf == list(i for i in word): 
-        print(f'\nYou did it! The word is "{word}"')
-        success = True
-    else:  
-        life-=1
-        print(f"Nope. Lifes left: {life}")
-        print("".join([i+" " for i in wordObf]))
+def debug(*argv):
+    for arg in argv: print(arg)
+    return
+
+def guessing(life, word):
+    wordObf = ['_' for i in list(word[:])]
+    success = False
+    while life>0 and not(success):
+
+        guess = input("Guess the letter: ")
+        print('\n')
+        if set(guess).issubset(set(word)):
+            counter1 = 0
+            for i in word:
+                if i == guess: 
+                    wordObf[counter1] = i
+                counter1+=1
+            print("Good guess!")
+            print("".join([i+" " for i in wordObf]))
+        else:  
+            life-=1
+            print(f"Nope. Lifes left: {life}")
+            print("".join([i+" " for i in wordObf]))
+        if wordObf == list(i for i in word): 
+            print(f'\nYou did it! The word is "{word}"')
+            success = True
+    if not(success): print(f"\nThe word was: {word}")        
+    return
+
+def main():
+    life = 6
+    wordSite = 'https://www.mit.edu/~ecprice/wordlist.10000'
+    response = urllib.request.urlopen(wordSite)
+    long_txt = response.read().decode()
+    words = long_txt.splitlines()
+    word = words[random.randrange(0,len(words))]
+    debug(response, word)
+
+
+    guessing(life, word)
+    return
+
+
+
+if __name__ == "__main__":
+    main()
