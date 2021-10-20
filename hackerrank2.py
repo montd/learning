6d3e48c168634bd7bd87117f2458fb8d54878f58
montd <dzejdzej.j@gmail.com>
2021-10-20 23:11:29 +0200

    Not yet working

--- hackerrank2.py
+++ hackerrank2.py
@@ -1,18 +1,23 @@
 # https://www.hackerrank.com/challenges/no-idea/problem
 
-def noIdea(n,m,set1,set2):
-    # get n,m -> wielkosc test-aginstr, wielkosc zbiorów A i B
+def noIdea(m,n,set1,set2):
     # get array of test-againsts
     # get set nr. 1
     # get set nr. 2
     # declare initial happiness
-    set1=set(set1)
-    set2=set(set2)
-    happy = 0
-    for i in 
+    set1=set(set1.split(' '))
+    set2=set(set2.split(' '))
+    m = set(m.split(' '))
+    print(len(set1 & m)-len(set2 & m))
     
 
 
 
 if __name__ == '__main__':
-    noIdea((3,2),(1,5,3),(3,1),(5,7))
+    # noIdea((3,2),(1,5,3),(3,1),(5,7))
+    f = open("input03.txt", "r")
+    # print(len(data.split('\n')))
+    noIdea(f.read().split('\n'))
+    f.close()
+
+    # noIdea(input(),input(),input(),input())
