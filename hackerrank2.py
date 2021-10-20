a59e0087db904764a551fe4f09fe0c2034bce16f
montd <dzejdzej.j@gmail.com>
2021-10-20 15:58:39 +0200

    Init

---
+++ hackerrank2.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/no-idea/problem
+
+def noIdea(n,m,set1,set2):
+    # get n,m -> wielkosc test-aginstr, wielkosc zbiorów A i B
+    # get array of test-againsts
+    # get set nr. 1
+    # get set nr. 2
+    # declare initial happiness
+    set1=set(set1)
+    set2=set(set2)
+    happy = 0
+    for i in 
+    
+
+
+
+if __name__ == '__main__':
+    noIdea((3,2),(1,5,3),(3,1),(5,7))
