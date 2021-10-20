c29e4eb5d3194864a0ae0403b2750e13588d2e00
montd <dzejdzej.j@gmail.com>
2021-10-20 15:35:44 +0200

    Solved

--- hackerscripts.py
+++ hackerscripts.py
@@ -1,15 +1,21 @@
 # https://www.hackerrank.com/challenges/merge-the-tools/problem
 
-def merge_the_tools(string, k):
+def merge_the_tools(theString, k):
     
-    n = len(string)
-    print(n)
-    substring = [[x for x in range(k)]for x in range(n//k)]
-    print(substring)
+    n = len(theString)
+    # print(n)
+    index = 0
+    sub = ['' for i in range(n//k)]
+    for x in range(n//k):
+        for y in range(k):
+            if (theString[index] not in sub[x]): sub[x] += theString[index]
+            index+=1
+        print(sub[x])
+    # print(sub, len(sub), len(sub[0]))
+
     # initialize substring[n/k][k] that would be a two dimensional list
     # initialize buffer type list of size(k)
-    test = {1, 3, 5, 1, 5, 1}
-    print(test)
+    
 
 if __name__ == '__main__':
     merge_the_tools("AABCAAADA", 3)
